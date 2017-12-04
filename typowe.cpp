043cea81dbe8221878ef420305b9cff4e236d63f
kutylamaciej <maciejo117@gmail.com>
2017-12-04 18:32:50 +0100

    Added new gooooood stuff

---
+++ typowe.cpp
@@ -0,0 +1,39 @@
+/* Skladnia programu w C */
+/* Komentarze */
+
+--
+Komentarze 
+-
+pocz¹tek
+#include
+<bibl.h> 
+-
+w³¹czenie biblioteki, np. <stdio.h>, <conio.h>
+#define
+NAZWA tekst 
+-
+definicja sta³ej symbolicznej, np. #define PI 3.145926
+--
+koniec komentarzy
+
+main()
+/* klamra otwieraj¹ca 
+-
+pocz¹tek funkcji g³ównej
+(jak begin w Pascalu)*/
+{
+/* tu zaczyna siê treœæ programu 
+-
+w funkcji main()*/
+/* treœæ */
+}
+/* klamra zamykaj¹ca 
+-
+jak end w Pascalu*/
+/* 
+-
+rozpoczêcie komentarza w C 
+-
+koniec komentarza
+*/
+// komentarz w C++
